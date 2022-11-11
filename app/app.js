e239adfd1710e2882e4091eaf99e81da44cffd9b
aidkose <aidkose@iu.edu>
2022-11-11 12:23:10 -0800

    Init repo
    Init Repo

---
+++ app/app.js
@@ -0,0 +1,200 @@
+var SPORTS = [
+ {
+  name: "Sports",
+  listItems: [
+   {
+    name: "Football",
+    checked: false,
+   },
+   {
+    name: "Soccer",
+    checked: false,
+   },
+   {
+    name: "Basketball",
+    checked: false,
+   },
+   {
+    name: "Baseball",
+    checked: false,
+   },
+   {
+    name: "Tennis",
+    checked: false,
+   },
+   {
+    name: "MMA",
+    checked: false,
+   },
+   {
+    name: "Boxing",
+    checked: false,
+   },
+   {
+    name: "Volleyball",
+    checked: false,
+   },
+   {
+    name: "Wrestling",
+    checked: false,
+   },
+   {
+    name: "Cricket",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Groceries",
+  listItems: [
+   {
+    name: "Cheese",
+    checked: false,
+   },
+   {
+    name: "Bread",
+    checked: false,
+   },
+   {
+    name: "Milk",
+    checked: false,
+   },
+   {
+    name: "Steak",
+    checked: false,
+   },
+   {
+    name: "Rice",
+    checked: false,
+   },
+   {
+    name: "Broccoli",
+    checked: false,
+   },
+   {
+    name: "Tomatos",
+    checked: false,
+   },
+   {
+    name: "Avacado",
+    checked: false,
+   },
+   {
+    name: "Coffee Mix",
+    checked: false,
+   },
+   {
+    name: "Lettuce",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Video Games",
+  listItems: [
+   {
+    name: "Total War: Warhammer 3",
+    checked: false,
+   },
+   {
+    name: "Metro Exodus",
+    checked: false,
+   },
+   {
+    name: "The Witcher 3: Wild Hunt",
+    checked: false,
+   },
+   {
+    name: "Team Fortress 2",
+    checked: false,
+   },
+   {
+    name: "Noita",
+    checked: false,
+   },
+   {
+    name: "Sid Meier's Civilization VI",
+    checked: false,
+   },
+   {
+    name: "Star Wars: Battlefront II",
+    checked: false,
+   },
+   {
+    name: "God of War",
+    checked: false,
+   },
+   {
+    name: "Total War: Warhammer 3",
+    checked: false,
+   },
+   {
+    name: "Total War: Warhammer 3",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Movies",
+  listItems: [
+   {
+    name: "John Wick",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Board Games",
+  listItems: [
+   {
+    name: "Sorry!",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Albums",
+  listItems: [
+   {
+    name: "Leviathan",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Colors",
+  listItems: [
+   {
+    name: "Red",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Software",
+  listItems: [
+   {
+    name: "Adobe XD",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "Genres",
+  listItems: [
+   {
+    name: "Metal",
+    checked: false,
+   },
+  ],
+ },
+ {
+  name: "D&D Classes",
+  listItems: [
+   {
+    name: "Artificer",
+    checked: false,
+   },
+  ],
+ },
+];

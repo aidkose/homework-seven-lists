147b7de0c2dca3533bef3a566391f78a6e709bd0
aidkose <aidkose@iu.edu>
2022-11-11 15:51:43 -0800

    got checks working
    for whatever reason, my checkboxes were hidden, but I figured out the issue.

--- app/app.js
+++ app/app.js
@@ -1,452 +1,496 @@
 var CATEGORIES = [
- {
-  name: "Sports",
-  listItems: [
-   {
-    name: "Football",
-    checked: false,
-   },
-   {
-    name: "Soccer",
-    checked: false,
-   },
-   {
-    name: "Basketball",
-    checked: false,
-   },
-   {
-    name: "Baseball",
-    checked: false,
-   },
-   {
-    name: "Tennis",
-    checked: false,
-   },
-   {
-    name: "MMA",
-    checked: false,
-   },
-   {
-    name: "Boxing",
-    checked: false,
-   },
-   {
-    name: "Volleyball",
-    checked: false,
-   },
-   {
-    name: "Wrestling",
-    checked: false,
-   },
-   {
-    name: "Cricket",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Groceries",
-  listItems: [
-   {
-    name: "Cheese",
-    checked: false,
-   },
-   {
-    name: "Bread",
-    checked: false,
-   },
-   {
-    name: "Milk",
-    checked: false,
-   },
-   {
-    name: "Steak",
-    checked: false,
-   },
-   {
-    name: "Rice",
-    checked: false,
-   },
-   {
-    name: "Broccoli",
-    checked: false,
-   },
-   {
-    name: "Tomatos",
-    checked: false,
-   },
-   {
-    name: "Avacado",
-    checked: false,
-   },
-   {
-    name: "Coffee Mix",
-    checked: false,
-   },
-   {
-    name: "Lettuce",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Video Games",
-  listItems: [
-   {
-    name: "Total War: Warhammer 3",
-    checked: false,
-   },
-   {
-    name: "Metro Exodus",
-    checked: false,
-   },
-   {
-    name: "The Witcher 3: Wild Hunt",
-    checked: false,
-   },
-   {
-    name: "Team Fortress 2",
-    checked: false,
-   },
-   {
-    name: "Noita",
-    checked: false,
-   },
-   {
-    name: "Sid Meier's Civilization VI",
-    checked: false,
-   },
-   {
-    name: "Star Wars: Battlefront II",
-    checked: false,
-   },
-   {
-    name: "God of War",
-    checked: false,
-   },
-   {
-    name: "Dark Souls",
-    checked: false,
-   },
-   {
-    name: "Uncharted",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Movies",
-  listItems: [
-   {
-    name: "John Wick",
-    checked: false,
-   },
-   {
-    name: "Mad Max: Fury Road",
-    checked: false,
-   },
-   {
-    name: "Shutter Island",
-    checked: false,
-   },
-   {
-    name: "Kill Bill",
-    checked: false,
-   },
-   {
-    name: "The Witch",
-    checked: false,
-   },
-   {
-    name: "The Northman",
-    checked: false,
-   },
-   {
-    name: "The Batman",
-    checked: false,
-   },
-   {
-    name: "The Black Phone",
-    checked: false,
-   },
-   {
-    name: "Gangs of New York",
-    checked: false,
-   },
-   {
-    name: "There Will be Blood",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Board Games",
-  listItems: [
-   {
-    name: "Sorry!",
-    checked: false,
-   },
-   {
-    name: "Checkers",
-    checked: false,
-   },
-   {
-    name: "Chess",
-    checked: false,
-   },
-   {
-    name: "Monopoly",
-    checked: false,
-   },
-   {
-    name: "Risk",
-    checked: false,
-   },
-   {
-    name: "Sorry!",
-    checked: false,
-   },
-   {
-    name: "Battleship",
-    checked: false,
-   },
-   {
-    name: "Candy Land",
-    checked: false,
-   },
-   {
-    name: "Backgammon",
-    checked: false,
-   },
-   {
-    name: "Chutes and Ladders",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Albums",
-  listItems: [
-   {
-    name: "Leviathan",
-    checked: false,
-   },
-   {
-    name: "May Our Chambers Be Full",
-    checked: false,
-   },
-   {
-    name: "Souvlaki",
-    checked: false,
-   },
-   {
-    name: "Homogenic",
-    checked: false,
-   },
-   {
-    name: "Sinner Get Ready",
-    checked: false,
-   },
-   {
-    name: "Songs for the Deaf",
-    checked: false,
-   },
-   {
-    name: "The Suburbs",
-    checked: false,
-   },
-   {
-    name: "You Fail Me",
-    checked: false,
-   },
-   {
-    name: "TR/ST",
-    checked: false,
-   },
-   {
-    name: "American Dream",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Colors",
-  listItems: [
-   {
-    name: "Red",
-    checked: false,
-   },
-   {
-    name: "Orange",
-    checked: false,
-   },
-   {
-    name: "Yellow",
-    checked: false,
-   },
-   {
-    name: "Green",
-    checked: false,
-   },
-   {
-    name: "Blue",
-    checked: false,
-   },
-   {
-    name: "Indigo",
-    checked: false,
-   },
-   {
-    name: "Violet",
-    checked: false,
-   },
-   {
-    name: "Pink",
-    checked: false,
-   },
-   {
-    name: "White",
-    checked: false,
-   },
-   {
-    name: "Black",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Software",
-  listItems: [
-   {
-    name: "Adobe XD",
-    checked: false,
-   },
-   {
-    name: "Visual Studio Code",
-    checked: false,
-   },
-   {
-    name: "Adobe Photoshop",
-    checked: false,
-   },
-   {
-    name: "Cyberduck",
-    checked: false,
-   },
-   {
-    name: "Blender",
-    checked: false,
-   },
-   {
-    name: "Slack",
-    checked: false,
-   },
-   {
-    name: "Steam",
-    checked: false,
-   },
-   {
-    name: "Discord",
-    checked: false,
-   },
-   {
-    name: "Zoom",
-    checked: false,
-   },
-   {
-    name: "Adobe Illustrator",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "Genres",
-  listItems: [
-   {
-    name: "Metal",
-    checked: false,
-   },
-   {
-    name: "Rock",
-    checked: false,
-   },
-   {
-    name: "Pop",
-    checked: false,
-   },
-   {
-    name: "Rap",
-    checked: false,
-   },
-   {
-    name: "Country",
-    checked: false,
-   },
-   {
-    name: "Electronic",
-    checked: false,
-   },
-   {
-    name: "Indie",
-    checked: false,
-   },
-   {
-    name: "Blues",
-    checked: false,
-   },
-   {
-    name: "Folk",
-    checked: false,
-   },
-   {
-    name: "Funk",
-    checked: false,
-   },
-  ],
- },
- {
-  name: "D&D Classes",
-  listItems: [
-   {
-    name: "Artificer",
-    checked: false,
-   },
-   {
-    name: "Barbarian",
-    checked: false,
-   },
-   {
-    name: "Fighter",
-    checked: false,
-   },
-   {
-    name: "Bard",
-    checked: false,
-   },
-   {
-    name: "Ranger",
-    checked: false,
-   },
-   {
-    name: "Rogue",
-    checked: false,
-   },
-   {
-    name: "Cleric",
-    checked: false,
-   },
-   {
-    name: "Paladin",
-    checked: false,
-   },
-   {
-    name: "Sorcerer",
-    checked: false,
-   },
-   {
-    name: "Wizard",
-    checked: false,
-   },
-  ],
- },
+  {
+    name: "Sports",
+    listItems: [
+      {
+        name: "Football",
+        checked: false,
+      },
+      {
+        name: "Soccer",
+        checked: false,
+      },
+      {
+        name: "Basketball",
+        checked: false,
+      },
+      {
+        name: "Baseball",
+        checked: false,
+      },
+      {
+        name: "Tennis",
+        checked: false,
+      },
+      {
+        name: "MMA",
+        checked: false,
+      },
+      {
+        name: "Boxing",
+        checked: false,
+      },
+      {
+        name: "Volleyball",
+        checked: false,
+      },
+      {
+        name: "Wrestling",
+        checked: false,
+      },
+      {
+        name: "Cricket",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Groceries",
+    listItems: [
+      {
+        name: "Cheese",
+        checked: false,
+      },
+      {
+        name: "Bread",
+        checked: false,
+      },
+      {
+        name: "Milk",
+        checked: false,
+      },
+      {
+        name: "Steak",
+        checked: false,
+      },
+      {
+        name: "Rice",
+        checked: false,
+      },
+      {
+        name: "Broccoli",
+        checked: false,
+      },
+      {
+        name: "Tomatos",
+        checked: false,
+      },
+      {
+        name: "Avacado",
+        checked: false,
+      },
+      {
+        name: "Coffee Mix",
+        checked: false,
+      },
+      {
+        name: "Lettuce",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Video Games",
+    listItems: [
+      {
+        name: "Total War: Warhammer 3",
+        checked: false,
+      },
+      {
+        name: "Metro Exodus",
+        checked: false,
+      },
+      {
+        name: "The Witcher 3: Wild Hunt",
+        checked: false,
+      },
+      {
+        name: "Team Fortress 2",
+        checked: false,
+      },
+      {
+        name: "Noita",
+        checked: false,
+      },
+      {
+        name: "Sid Meier's Civilization VI",
+        checked: false,
+      },
+      {
+        name: "Star Wars: Battlefront II",
+        checked: false,
+      },
+      {
+        name: "God of War",
+        checked: false,
+      },
+      {
+        name: "Dark Souls",
+        checked: false,
+      },
+      {
+        name: "Uncharted",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Movies",
+    listItems: [
+      {
+        name: "John Wick",
+        checked: false,
+      },
+      {
+        name: "Mad Max: Fury Road",
+        checked: false,
+      },
+      {
+        name: "Shutter Island",
+        checked: false,
+      },
+      {
+        name: "Kill Bill",
+        checked: false,
+      },
+      {
+        name: "The Witch",
+        checked: false,
+      },
+      {
+        name: "The Northman",
+        checked: false,
+      },
+      {
+        name: "The Batman",
+        checked: false,
+      },
+      {
+        name: "The Black Phone",
+        checked: false,
+      },
+      {
+        name: "Gangs of New York",
+        checked: false,
+      },
+      {
+        name: "There Will be Blood",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Board Games",
+    listItems: [
+      {
+        name: "Sorry!",
+        checked: false,
+      },
+      {
+        name: "Checkers",
+        checked: false,
+      },
+      {
+        name: "Chess",
+        checked: false,
+      },
+      {
+        name: "Monopoly",
+        checked: false,
+      },
+      {
+        name: "Risk",
+        checked: false,
+      },
+      {
+        name: "Sorry!",
+        checked: false,
+      },
+      {
+        name: "Battleship",
+        checked: false,
+      },
+      {
+        name: "Candy Land",
+        checked: false,
+      },
+      {
+        name: "Backgammon",
+        checked: false,
+      },
+      {
+        name: "Chutes and Ladders",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Albums",
+    listItems: [
+      {
+        name: "Leviathan",
+        checked: false,
+      },
+      {
+        name: "May Our Chambers Be Full",
+        checked: false,
+      },
+      {
+        name: "Souvlaki",
+        checked: false,
+      },
+      {
+        name: "Homogenic",
+        checked: false,
+      },
+      {
+        name: "Sinner Get Ready",
+        checked: false,
+      },
+      {
+        name: "Songs for the Deaf",
+        checked: false,
+      },
+      {
+        name: "The Suburbs",
+        checked: false,
+      },
+      {
+        name: "You Fail Me",
+        checked: false,
+      },
+      {
+        name: "TR/ST",
+        checked: false,
+      },
+      {
+        name: "American Dream",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Colors",
+    listItems: [
+      {
+        name: "Red",
+        checked: false,
+      },
+      {
+        name: "Orange",
+        checked: false,
+      },
+      {
+        name: "Yellow",
+        checked: false,
+      },
+      {
+        name: "Green",
+        checked: false,
+      },
+      {
+        name: "Blue",
+        checked: false,
+      },
+      {
+        name: "Indigo",
+        checked: false,
+      },
+      {
+        name: "Violet",
+        checked: false,
+      },
+      {
+        name: "Pink",
+        checked: false,
+      },
+      {
+        name: "White",
+        checked: false,
+      },
+      {
+        name: "Black",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Software",
+    listItems: [
+      {
+        name: "Adobe XD",
+        checked: false,
+      },
+      {
+        name: "Visual Studio Code",
+        checked: false,
+      },
+      {
+        name: "Adobe Photoshop",
+        checked: false,
+      },
+      {
+        name: "Cyberduck",
+        checked: false,
+      },
+      {
+        name: "Blender",
+        checked: false,
+      },
+      {
+        name: "Slack",
+        checked: false,
+      },
+      {
+        name: "Steam",
+        checked: false,
+      },
+      {
+        name: "Discord",
+        checked: false,
+      },
+      {
+        name: "Zoom",
+        checked: false,
+      },
+      {
+        name: "Adobe Illustrator",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Genres",
+    listItems: [
+      {
+        name: "Metal",
+        checked: false,
+      },
+      {
+        name: "Rock",
+        checked: false,
+      },
+      {
+        name: "Pop",
+        checked: false,
+      },
+      {
+        name: "Rap",
+        checked: false,
+      },
+      {
+        name: "Country",
+        checked: false,
+      },
+      {
+        name: "Electronic",
+        checked: false,
+      },
+      {
+        name: "Indie",
+        checked: false,
+      },
+      {
+        name: "Blues",
+        checked: false,
+      },
+      {
+        name: "Folk",
+        checked: false,
+      },
+      {
+        name: "Funk",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "D&D Classes",
+    listItems: [
+      {
+        name: "Artificer",
+        checked: false,
+      },
+      {
+        name: "Barbarian",
+        checked: false,
+      },
+      {
+        name: "Fighter",
+        checked: false,
+      },
+      {
+        name: "Bard",
+        checked: false,
+      },
+      {
+        name: "Ranger",
+        checked: false,
+      },
+      {
+        name: "Rogue",
+        checked: false,
+      },
+      {
+        name: "Cleric",
+        checked: false,
+      },
+      {
+        name: "Paladin",
+        checked: false,
+      },
+      {
+        name: "Sorcerer",
+        checked: false,
+      },
+      {
+        name: "Wizard",
+        checked: false,
+      },
+    ],
+  },
 ];
+
+function itemChecked(element, listIndex, itemIndex) {
+  $(element).parent().toggleClass("strike");
+  let checkedValue = !CATEGORIES[listIndex].listItems[itemIndex].checked;
+  CATEGORIES[listIndex].listItems[itemIndex].checked = checkedValue;
+}
+
+function loadListItems(listIndex) {
+  let listString = `<button onclick="loadLists()">Go Back</button><ul>`;
+  $.each(CATEGORIES[listIndex].listItems, function (idx, listItem) {
+    listString += `<li id="${idx}" class=${
+      listItem.checked ? "strike" : ""
+    }><input ${
+      listItem.checked ? (checked = "checked") : ""
+    } type="checkbox" id="${idx}" name="${
+      listItem.name
+    }" onclick="itemChecked(this, ${listIndex}, ${idx})">${listItem.name}</li>`;
+  });
+  listString += "</ul>";
+  $("#app").html(listString);
+}
+
+/**
+ * It takes the CATEGORIES array and creates a string of HTML that is then inserted into the #app
+ * element.
+ */
+function loadLists() {
+  let listString = "<ul>";
+  $.each(CATEGORIES, function (idx, mainList) {
+    listString += `<li id="${idx}" onclick="loadListItems(${idx})">${mainList.name}</li>`;
+  });
+  listString += "</ul>";
+  $("#app").html(listString);
+}
+
+/**
+ * When the document is ready, initialize the listeners and load the list items.
+ */
+function initListeners() {}
+
+$(document).ready(function () {
+  initListeners();
+  loadLists();
+});
